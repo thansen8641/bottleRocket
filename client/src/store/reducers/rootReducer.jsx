4bd7efb9c0adef9727025d3335b58d325ee2c90b
Timothy Hansen <timothyhansen229@gmail.com>
2021-10-05 10:26:58 -0700

    changes

--- client/src/store/reducers/rootReducer.jsx
+++ client/src/store/reducers/rootReducer.jsx
@@ -1,25 +1,9 @@
 import React from 'react';
-
-var calculatedInterest;
-
-const initState = {
-  calculatedInterest,
-}
+import { combineReducers } from 'redux'
 
 
-const rootReducer = (state = initState, action ) => {
-  switch (action.type) {
-    case "SET_CALCULATED_INTEREST":
-    var toReturn = {...state}
-    toReturn.calculatedInterest = action.amount
-    return toReturn
-    case "CLEAR_CALCULATED_INTEREST":
-    var toReturn = {...state}
-    toReturn.calculatedInterest = null
-    return toReturn
-    default:
-    return state
-  }
-}
 
-export default rootReducer;
+
+export default (history) => combineReducers({
+
+});
