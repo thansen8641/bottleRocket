047128411be6d9c7aa617158557c290a2fd024b9
Timothy Hansen <timothyhansen229@gmail.com>
2021-10-07 12:39:07 -0700

    added readme and some smaller changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header.jsx'
 import ProductList from './components/ProductList.jsx'
 import { connect } from "react-redux";
